847415e9fe7ea3b15d531bacd6db1d993efd0b33
albanjin <albanjin@163.com>
2019-03-21 00:47:46 +0800

    first commit

--- config/config.base.js
+++ config/config.base.js
@@ -1,8 +1,12 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 module.exports = {
-    entry:{"Index":"./src/pages/Index/index.js","Other":"./src/pages/Other/index.js","three":"./src/pages/three.js"},
+    entry:{"333":"./src/pages/333/index.js","Index":"./src/pages/Index/index.js","Other":"./src/pages/Other/index.js","three":"./src/pages/three.js"},
     plugins : [new HtmlWebpackPlugin({
+            template:'./index.html',
+            filename: './333.html',
+            chunks:["333"]
+        }),new HtmlWebpackPlugin({
             template:'./index.html',
             filename: './Index.html',
             chunks:["Index"]
